ed67ad59940b895fe1e20cc2c92574b342d325df
Robert Allen <robby.allen24@gmail.com>
2016-11-04 15:28:23 -0400

    some work on a custom model selector

--- src/pages/predict-page.jsx
+++ src/pages/predict-page.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 
 import { RouteActions } from '../actions';
 import { routes } from '../constants';
 
 class PredictPage extends React.Component {
+
+	constructor(){
+		super();
+		let models = ['Animal', 'Food', 'Sport', 'Movie'];
+		this.state = {
+			models
+		};
+		this.modelChange = this.modelChange.bind(this);
+	}
+
 	componentWillMount(){
 		this.props.dispatch(RouteActions.setAppRoute(routes.PREDICT));
 	}
 
+	modelChange(val){
+		console.log(val)
+		this.setState({currentModel: val});
+	}
+
 	render () {
+
+		let models = this.state.models || []
+		let options = models.map((model) => {
+			return {
+				value: model,
+				label: model
+			}
+		})
+
 		return (
 			<div className="predict-page">
 				<h1>Define features</h1>
@@ -19,10 +45,14 @@ class PredictPage extends React.Component {
 				<p><code>5500,2.9</code></p>
 
 				<div>
-					<form method="POST" action="">
-						<p><select name="model">
-								<option value="filename">filename</option>
-						</select></p>
+					<form>
+						<Select
+								className="react-select"
+								name="selectModels"
+								value={this.state.currentModel}
+								options={options}
+								onChange={this.modelChange}
+						/>
 						<p><input type="text" name="data" /></p>
 						<p><input type="submit" className="button" value="submit"/></p>
 					</form>
